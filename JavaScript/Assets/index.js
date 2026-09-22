9f2ce3880416fac5c630059f0e85467e4acf5307
SARJITH2004 <119404894+SARJITH2004@users.noreply.github.com>
2026-09-22 23:39:01 +0530

    jsdayone

---
+++ JavaScript/Assets/index.js
@@ -0,0 +1,12 @@
+console.log(1);
+console.log("String");
+console.error("Are you sure");
+console.warn("seriously");
+document.write("text");
+console.log(1==1);
+console.log(true);
+console.log("true+3 =",true+3,"false+3 =",false+3);
+console.log("this is due to the fact that the value of true is 1 and false is 0");
+document.write("text two")
+var uniqueClass = "variable"
+console.log(uniqueClass);
