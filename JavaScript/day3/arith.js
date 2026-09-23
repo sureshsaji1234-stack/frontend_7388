99d6b60eb07330b9f6109a92dd0489f9db07c4ed
SARJITH2004 <119404894+SARJITH2004@users.noreply.github.com>
2026-09-24 00:09:48 +0530

    jsdayThree

---
+++ JavaScript/day3/arith.js
@@ -0,0 +1,52 @@
+let firstNumber=1
+let secondNumber=2
+let thirdNumber=3
+let fourthNumber=4
+
+let totalNumber=firstNumber=secondNumber=thirdNumber=fourthNumber
+console.log(totalNumber);
+console.log(firstNumber);
+console.log(secondNumber);
+console.log(thirdNumber);
+console.log(fourthNumber);
+
+// if(firstNumber ==secondNumber){
+//     firstNumber=5
+// }
+// else{
+//     firstNumber=6
+// }
+// console.log(firstNumber)
+
+// if(firstNumber ==secondNumber){
+//     firstNumber=5
+// }
+// else{
+//     firstNumber=6
+// }
+// console.log(firstNumber)
+
+// for (let index = 0; index < 9; index++) {
+//     firstNumber+=1
+    
+// }
+// console.log(firstNumber);
+let firstString = "string"
+let secStr="con"
+console.log(firstString-secStr)
+let thirdStr="100"
+let fourthStr="55"
+let conStr=thirdStr-fourthStr
+console.log(conStr);
+let fifthStr="1000"
+let sixthStr="505"
+fpsStr=fifthStr-sixthStr
+console.log(fpsStr)
+
+const consValue="ReactJs"
+let trainString="train"
+let testString="test"
+let valString=trainString+testString
+console.log(valString);
+
+
